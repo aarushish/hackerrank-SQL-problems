8c086e7cf1d727c7f6bcb6171620e6b8012daef5
aarushish <46043620+aarushish@users.noreply.github.com>
2018-12-27 21:07:32 +0530

    Solution to Type of Triangle problem

---
+++ type_of_triangle.sql
@@ -0,0 +1,10 @@
+##https://www.hackerrank.com/challenges/what-type-of-triangle/problem
+
+Select
+ CASE 
+   WHEN A + B <= C OR A + C <= B OR B + C <= A THEN 'Not A Triangle'
+   WHEN A = B AND B = C THEN 'Equilateral'
+   WHEN A = B OR A = C OR B = C THEN 'Isosceles'
+   ELSE 'Scalene'
+ END
+FROM TRIANGLES;
